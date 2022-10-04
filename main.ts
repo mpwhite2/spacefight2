21b6662d6e9082e892fe9a6aec5a83ae359443a3
mpwhite2 <110784371+mpwhite2@users.noreply.github.com>
2022-10-03 17:26:25 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,14 +12,37 @@ enum ActionKind {
 function rotatePlayer (playerSprite: Sprite, direction: number) {
     transformSprites.changeRotation(playerSprite, direction)
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    doSomething(Player1)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     rotatePlayer(Player1, -30)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     rotatePlayer(Player1, 30)
 })
+controller.player2.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    rotatePlayer(Player2, 30)
+})
+function doSomething (mySprite: Sprite) {
+    let Thruster_V = 0
+    ThrustDir = transformSprites.getRotation(mySprite) - 90
+    ThrustDirRads = ThrustDir * 3.1416 / 180
+    ThrustX = Thruster_V * Math.cos(ThrustDirRads)
+    ThrustY = Thruster_V * Math.sin(ThrustDirRads)
+    mySprite.vx += ThrustX
+    mySprite.vy += ThrustY
+}
+controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    rotatePlayer(Player2, -30)
+})
+let ThrustY = 0
+let ThrustX = 0
+let ThrustDirRads = 0
+let ThrustDir = 0
 let Player1: Sprite = null
-let Player2 = sprites.create(img`
+let Player2: Sprite = null
+Player2 = sprites.create(img`
     . . . . . . . . 7 . . . . . . . 
     . . . . . . . . 7 . . . . . . . 
     . . . . . . . . 7 . . . . . . . 
@@ -38,25 +61,25 @@ let Player2 = sprites.create(img`
     . . . . 7 7 f 7 f 7 f 7 7 . . . 
     `, SpriteKind.Player)
 Player1 = sprites.create(img`
-    . . . . . . . 8 8 . . . . . . . 
-    . . . . . . . 8 8 . . . . . . . 
-    . . . . . . . 8 8 . . . . . . . 
-    . . . . . . 8 8 8 8 . . . . . . 
-    . . . . . . 8 8 8 8 . . . . . . 
-    . . . . . 8 8 8 8 8 8 . . . . . 
-    . . . . . 8 8 8 8 8 8 . . . . . 
-    . . . . 8 8 f f f f 8 8 . . . . 
-    . . . . 8 8 f f f f 8 8 . . . . 
-    . . . . 8 8 f f f f 8 8 . . . . 
-    . . . . 8 8 f f f f 8 8 . . . . 
-    . . . 8 8 8 8 8 8 8 8 8 8 . . . 
-    . . . 8 8 8 8 8 8 8 8 8 8 . . . 
-    . . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
-    . 8 8 8 8 f f 8 8 f f 8 8 8 8 . 
-    . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 7 . . . . . . . . 
+    . . . . . . . 8 . . . . . . . . 
+    . . . . . . . 8 . . . . . . . . 
+    . . . . . . 8 8 8 . . . . . . . 
+    . . . . . . 8 f 8 . . . . . . . 
+    . . . . . . 8 f 8 . . . . . . . 
+    . . . . . . 8 f 8 . . . . . . . 
+    . . . . . . 8 f 8 . . . . . . . 
+    . . . . . 8 8 8 8 8 . . . . . . 
+    . . . . 8 8 8 8 8 8 8 . . . . . 
+    . . . 8 2 8 8 8 8 2 8 8 . . . . 
+    . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
+    . 8 8 8 8 8 8 8 8 8 8 8 8 8 . . 
     `, SpriteKind.Player)
 MakeyMakey.setSimulatorKeymap(
-MakeyMakey.PlayerNumber.ONE,
+MakeyMakey.PlayerNumber.TWO,
 MakeyMakey.MakeyMakeyKey.W,
 MakeyMakey.MakeyMakeyKey.S,
 MakeyMakey.MakeyMakeyKey.A,
